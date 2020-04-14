81fa22419361c7742b87744e7623895bc54fd4fb
Jude Martin <judemartin.mj@gmail.com>
2020-04-14 09:42:35 -0700

    login with token implementation

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const postsRoutes = require('./routes/posts');
 const userRoutes = require('./routes/user');
 const path = require('path');
-mongoose.connect('mongodb+srv://admin:QSp897CTgjSIDfEY@cluster0-3nieq.azure.mongodb.net/angular-dryrun')
+mongoose.connect('mongodb+srv://admin:QSp897CTgjSIDfEY@cluster0-3nieq.azure.mongodb.net/angular-dryrun', { useNewUrlParser: true })
         .then(() => {
             console.log('connected to the database');
         })
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers', 
-        'Origin, X-Requested-With, Content-Type, Accept');
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader(
         'Access-Control-Allow-Methods',
         'GET, POST, PUT, PATCH, DELETE, OPTIONS');    
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.post("/signup", (req, res, next) => {
-    console.log(req);
+router.post("/signup", (req, res, next) => { 
     bcrypt.hash(req.body.password, 10)
-    .then(hash => { 
-        console.log(hash);
+    .then(hash => {  
         const user = new User({
             email: req.body.email,
             password: hash
@@ -26,6 +25,41 @@ router.post("/signup", (req, res, next) => {
         });
     });
 });
+
+router.post("/login", (req, res, next) => {
+    let fetchedUser;
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if(!user) {
+            return res.status(401).json({
+                message: "Auth failed"
+            });
+        }
+        fetchedUser = user;
+        return bcrypt.compare(req.body.password, user.password);  
+    })
+    .then(result => { 
+        if(!result) {
+            return res.status(401).json({
+                message: "Auth failed"
+            });
+        }
+        const token = jwt.sign(
+            { email: fetchedUser.email, userId: fetchedUser._id },
+            "secret_this_should_be_longer",
+            { expiresIn: "1h" }
+        ); 
+        res.status(200).json({
+            token: token
+        });
+    })
+    .catch(err => { 
+        return res.status(401).json({
+            message: "Auth failed"
+        });
+    });
+});
+
 
 
 module.exports = router;
