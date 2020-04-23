274b1ad67b353293236f87f104824c4cb4cbbeff
Jude Martin <judemartin.mj@gmail.com>
2020-04-23 08:54:52 -0700

    angular material dialog

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -6,22 +6,23 @@ import { MatDialog } from "@angular/material/dialog";
 import { ErrorComponent } from "./error/error/error.component";
 
 @Injectable()
-export class ErrorInterceptor implements HttpInterceptor { 
-    
+export class ErrorInterceptor implements HttpInterceptor {
+
     constructor(private dialog: MatDialog) {
-        
-        
+
+
     }
-    intercept(req: HttpRequest<any>, next: HttpHandler) { 
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
         return next.handle(req)
-        .pipe(
-           catchError((error: HttpErrorResponse) => {
-                console.log(error);
-                this.dialog.open(ErrorComponent);
-                //console.log(error);
-                alert(error.error.error.message);
-                return throwError(error);
-           })
-        );
-    } 
+            .pipe(
+                catchError((error: HttpErrorResponse) => {
+                    let errorMessage = 'An unknown error occured';
+                    if (error.error.message) {
+                        errorMessage = error.error.message;
+                    }
+                    this.dialog.open(ErrorComponent, { data: { message: errorMessage}});
+                    return throwError(error);
+                })
+            );
+    }
 }
--- src/app/error/error/error.component.ts
+++ src/app/error/error/error.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-@Component({ 
-  templateUrl: './error.component.html', 
+@Component({
+  templateUrl: './error.component.html',
 })
 export class ErrorComponent {
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {message: string}) { 
+  }
   message = 'An unknown error occured';
-  
+
 }
