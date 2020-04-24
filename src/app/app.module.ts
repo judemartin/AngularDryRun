057c60037d472535d52ba01b0bdffa83e17922de
Jude Martin <judemartin.mj@gmail.com>
2020-04-24 08:59:34 -0700

    split app into smaller features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AppComponent } from "./app.component";
-import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
-import { PostListComponent } from "./posts/post-list/post-list.component";
-import { RouterModule } from "@angular/router";
 import { AppRoutingModule } from "./app-routing.module";
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from "./auth/signup/signup.component";
@@ -15,13 +12,12 @@ import { AuthInterceptor } from "./auth/auth-interceptor";
 import { ErrorInterceptor } from "./error-interceptor";
 import { ErrorComponent } from "./error/error/error.component";
 import { AngularMaterialModule } from "./angular-material.module";
+import { PostsModule } from "./posts/posts.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostCreateComponent,
+    AppComponent, 
     HeaderComponent,
-    PostListComponent,
     LoginComponent,
     SignupComponent, 
     ErrorComponent
@@ -29,11 +25,11 @@ import { AngularMaterialModule } from "./angular-material.module";
   imports: [
     AppRoutingModule,
     BrowserModule,
-    ReactiveFormsModule,
     BrowserAnimationsModule,
     FormsModule, 
     HttpClientModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    PostsModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
